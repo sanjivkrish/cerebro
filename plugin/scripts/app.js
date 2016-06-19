0877a0f7d73e1bc7c20c62258e96a62257a627a8
sanjivkrish <sanjiv436.k@gmail.com>
2016-06-19 16:18:24 +0530

    plugin: app: Make a call for default world news
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- plugin/scripts/app.js
+++ plugin/scripts/app.js
@@ -1,8 +1,31 @@
 angular.module('cerebro', [])
 .controller('mainCtrl', function($scope, $http) {
+    //
+    //
+    //
+    var serverAddress = 'http://localhost:3333';
+
+    //
+    // Top menus - categories of news
+    //
     $scope.menus = [
         {display : 'Sports', api : 'sports'},
         {display : 'Films', api : 'film'},
         {display : 'Technology', api : 'tech'}
     ];
+
+    //
+    // Array of news content to be displayed
+    //
+    $scope.newsContent = [];
+
+    //
+    // Make a call for 'World' news
+    //
+    $http.get(serverAddress + '/world').then(function(res) {
+        console.log(res.data);
+        $scope.newsContent = res.data;
+    }, function(err) {
+        console.log('API news/world failed : ', err);
+    });
 });
