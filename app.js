92747ef58db6e111b36b940f695dadb72087d3a9
sanjivkrish <sanjiv436.k@gmail.com>
2016-06-19 17:12:46 +0530

    server: app: Allow request from other origin
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- app.js
+++ app.js
@@ -25,6 +25,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//
+// Request from cross domain needs to be responded with special headers
+//
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', routes);
 app.use('/world',world);
 app.use('/technology',technology);
